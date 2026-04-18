ebf902fe478de74060183aef2ef63e3a8fe517ed
mooonu <moonudev@gmail.com>
2026-04-19 02:26:10 +0900

    Initial Commit

---
+++ damoa/damoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  damoaApp.swift
+//  damoa
+//
+//  Created by 김승현 on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct damoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
